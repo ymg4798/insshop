4a5639922b828ba941471712f6a634eb54708ead
ymk <ymg4798@naver.com>
2023-06-05 14:56:24 +0900

    쇼핑몰 프로젝트 환경 설정

---
+++ src/main/java/com/insshop/InsshopApplication.java
@@ -0,0 +1,13 @@
+package com.insshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsshopApplication.class, args);
+    }
+
+}
